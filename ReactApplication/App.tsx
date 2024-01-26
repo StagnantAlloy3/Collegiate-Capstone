a8627cabffd9e05812485b894d462778c2c56bcd
Augustine <stagnantalloy3@gmail.com>
2024-01-26 14:40:24 -0800

    Warnings fix.

--- ReactApplication/App.tsx
+++ ReactApplication/App.tsx
@@ -5,114 +5,137 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer} from "@react-navigation/native";
+import {createMaterialBottomTabNavigator} from "@react-navigation/material-bottom-tabs";
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+function Home() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "black"}}>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
+
+function DetailsScreen() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Details Screen</Text>
+        </View>
+    );
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const theme = useState(isDarkMode ? darkTheme : lightTheme);
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    const Tab = createMaterialBottomTabNavigator();
+
+    return (
+        <NavigationContainer>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                       backgroundColor={theme.colors.notification}/>
+            <Tab.Navigator inactiveColor={theme.colors.secondary} activeColor={theme.colors.primary} barStyle={{backgroundColor: theme.colors.border}}>
+                <Tab.Screen name="Home" component={Home}
+                            options={{
+                                tabBarLabel: 'Home',
+                                tabBarIcon: () => (<MaterialIcons name="home" color={theme.colors.card} size={26}/>),
+                                tabBarBadge: 3
+                            }}
+                />
+                <Tab.Screen name="Details" component={DetailsScreen} options={{
+                    tabBarLabel: 'Details',
+                    tabBarIcon: () => (<MaterialIcons name="info" color="blue" size={26}/>),
+                }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+        /*<SafeAreaView style={backgroundStyle}>
+          <StatusBar
+            barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+            backgroundColor={backgroundStyle.backgroundColor}
+          />
+          <Header />
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={backgroundStyle}>
+            <View
+              style={{
+                backgroundColor: isDarkMode ? Colors.black : Colors.white,
+              }}>
+              <Section title="Step One">
+                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+                screen and then come back to see your edits.
+              </Section>
+              <Section title="See Your Changes">
+                <ReloadInstructions />
+              </Section>
+              <Section title="Debug">
+                <DebugInstructions />
+              </Section>
+              <Section title="Learn More">
+                Read the docs to discover what to do next:
+              </Section>
+              <LearnMoreLinks />
+            </View>
+          </ScrollView>
+        </SafeAreaView>*/
+    );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+/*const styles = StyleSheet.create({
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+});*/
+
+const lightTheme = {
+    dark: false,
+    colors: {
+        primary: '#3A356E',
+        background: '#00F4CC',
+        card: '#F5F549',
+        text: '#000000',
+        border: '#EFB7FF',
+        secondary: '#4F81F7',
+    },
+};
+
+const darkTheme = {
+    dark: false,
+    colors: {
+        primary: 'rgb(45,104,255)',
+        background: 'rgba(242,242,242,0.29)',
+        card: 'rgba(255,255,255,0.44)',
+        text: 'rgb(28, 28, 30)',
+        border: 'rgba(199,199,204,0.6)',
+        notification: 'rgb(90,236,7)',
+        secondary: 'rgb(28, 28, 30)',
+    },
+}
 
 export default App;
