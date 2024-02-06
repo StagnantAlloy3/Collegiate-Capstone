1e1361330f2da1839cf447cf4f2a4fd2efdacea1
Augustine <stagnantalloy3@gmail.com>
2024-02-06 09:36:32 -0800

    Updated ManualItemSearch.tsx to use a React hook with a timeout of 1 second to avoid rapid requests.  Updated ByName.java with the mongoDB connection and basic query for pulling food description and fdc_id (which will be used for pulling from another table).  Also updated Instructions.md with further adb descriptions.

--- FoodAPI/src/main/java/com/collins/augustine/foodapi/items/ByName.java
+++ FoodAPI/src/main/java/com/collins/augustine/foodapi/items/ByName.java
@@ -1,5 +1,6 @@
 package com.collins.augustine.foodapi.items;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import javax.ws.rs.GET;
@@ -8,6 +9,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCursor;
+import org.bson.Document;
+
 @Path("/items/by-name")
 public class ByName {
 
@@ -15,9 +23,40 @@ public class ByName {
     @Produces(MediaType.APPLICATION_JSON)
     public String byName(@QueryParam("itemname") String name) {
 
-        JsonObject item = new JsonObject();
-        item.addProperty("name", name);
+        JsonArray items = new JsonArray();
 
-        return item.toString();
+        ConnectionString connString = new ConnectionString("mongodb://localhost:27017/Foods");
+        try{
+            MongoClient mongoClient = MongoClients.create(connString);
+            System.out.println("Connected to MongoDB");
+
+            Document regex = new Document();
+            regex.put("$regex", "^(?)" + name);
+            regex.put("$options", "i");
+
+            Document searchItem = new Document();
+            searchItem.put("description", regex);
+
+            FindIterable<Document> foundItems = mongoClient.getDatabase("Foods").getCollection("Food").find(searchItem);
+
+            try(final MongoCursor<Document> cursor = foundItems.iterator()) {
+                while(cursor.hasNext()) {
+                    Document doc = cursor.next();
+                    JsonObject item = new JsonObject();
+                    item.addProperty("fdc_id", doc.getInteger("fdc_id"));
+                    item.addProperty("description", doc.getString("description"));
+                    items.add(item);
+                }
+            }
+
+            mongoClient.close();
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+        System.out.println("Items: " + items);
+
+        return items.toString();
     }
 }
